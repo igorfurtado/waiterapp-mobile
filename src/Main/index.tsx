4b1571e9839129fd1284413445d4906e6478914b
Igor Targino <igortargino@icloud.com>
2024-01-05 18:04:53 -0300

    feat: iniciando a criacao do primeiro componente

---
+++ src/Main/index.tsx
@@ -0,0 +1,12 @@
+import { Text } from '../components/Text'
+import { Container } from './styles'
+
+const Main = () => {
+  return (
+    <Container>
+      <Text>Main renderizou</Text>
+    </Container>
+  )
+}
+
+export default Main
